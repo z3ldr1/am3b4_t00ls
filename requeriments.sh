7c8cb26e9a0960632f4e78f3d16353e9fa08519b
z3ldr1s <128452420+z3ldr1@users.noreply.github.com>
2025-01-15 13:15:49 -0300

    Update requeriments.sh

--- requeriments.sh
+++ requeriments.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 #variaveis
 c="clear"
+i="sudo apt-get install "
+p="sudo pip3 install"
+up="sudo apt-get update -y && sudo apt-get full-upgrade -y"
+b="--break-system-package"
 
-
-echo " "
 echo "[**Atualizando arquivos...**]"
-sudo apt-get update -y && sudo apt-get full-upgrade -y
+$up
 echo " 
 [**Atualização completa**] 
 $c
@@ -15,32 +17,41 @@ echo "[**Baixando dependências do Ameba...**]
 "
 
 # Instalação de pacotes
-
-  sudo apt-ge install python2 -y 
-   sudo apt-get install python3 -y 
-     sudo apt-get install git -y 
-     sudo apt-get install nmap -y 
-    sudo apt-get install nikto -y 
-    sudo apt-get install whatweb -y 
-   sudo apt-get install  nuclei -y 
-   sudo apt-get install  subfinder -y 
-   sudo apt-get install  gobuster -y 
-    sudo apt-get install wpscan -y 
-   sudo apt-get install  sherlock -y 
-   sudo apt-get install  proxychains4 -y 
-   sudo apt-get install  ettercap -y 
-   sudo apt-get install  wget -y 
- sudo apt-get install  whois -y
+$i python3-pip -y
+$i python2 -y 
+$i python3 -y 
+$i git -y 
+$i nmap -y 
+$i nikto -y 
+$i whatweb -y 
+$i nuclei -y 
+$i subfinder -y 
+$i gobuster -y 
+$i wpscan -y 
+$i sherlock -y 
+$i proxychains4 -y 
+$i ettercap -y 
+$i wget -y 
+$i whois -y
+$i veil -y
+$i hashcat -y
+$i wordlists -y
+$i setoolkit -y
 $c
 
 #Instalando uro
-echo "Fazendo o donwload do uro."
-sudo pip3 install uro --break-system-packages
-echo "
-[**Download do uro finalizado!**]
+echo "[**Fazendo o donwload do uro.**]"
+$p uro $b
+echo "[**Fazendo download de ferramentas de Phishing**]"
+$p maxphisher $b
+$p colorama $b
+$p sockets $b
+$p requests $b
 
-"
+echo "
+[**Download do uro finalizado!**] "
 $c
+$p 
 # Clonando repositório NucleiFuzzer e executando instalação
 if [ ! -d "NucleiFuzzer" ]; then
   sudo git clone https://github.com/0xKayala/NucleiFuzzer.git
@@ -50,15 +61,9 @@ if [ ! -d "NucleiFuzzer" ]; then
 else
   echo "[**Repositório NucleiFuzzer já clonado.**]"
 fi
-
-echo " 
-
-"
+$c
 echo "[**Dependências baixadas com sucesso**]"
-
-echo "
- 
-"
-sudo apt-get update -y && sudo apt-get full-upgrade -y
+$c
+$up
 $c
 echo "[**A atualização foi concluída com sucesso**]"
