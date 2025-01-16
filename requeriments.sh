26d863942552eeb84e147f54edbb833667fbb4c4
z3ldr1s <128452420+z3ldr1@users.noreply.github.com>
2025-01-15 21:47:24 -0300

    Update requeriments.sh

--- requeriments.sh
+++ requeriments.sh
@@ -5,15 +5,7 @@ i="sudo apt-get install "
 p="sudo pip3 install"
 up="sudo apt-get update -y && sudo apt-get full-upgrade -y"
 b="--break-system-package"
-ngrok="curl -sSL https://ngrok-agent.s3.amazonaws.com/ngrok.asc \
-	| sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null \
-	&& echo "deb https://ngrok-agent.s3.amazonaws.com buster main" \
-	| sudo tee /etc/apt/sources.list.d/ngrok.list \
-	&& sudo apt update \
-	&& sudo apt install ngrok"
- m="curl -O "https://bitbucket.org/iBotPeaches/apktool/downloads/apktool_2.10.0.jar" > apktool.jar && chmod +x apktool.jar
-mv apktool.jar /usr/local/bin && curl -O "https://raw.githubusercontent.com/iBotPeaches/Apktool/master/scripts/linux/apktool" > apktool
-chmod +x apktool && mv apktool /usr/local/bin"
+px="pipx install xyz"
 
 echo "[**Atualizando arquivos...**]"
 $up
@@ -52,7 +44,9 @@ $i postgresql -y
 $i apache2 -y
 $i tor -y
 $i veil -y
-
+$i apktool -y
+$i metagoofil -y
+$i make -y
 $c
 
 #Instalando uro
@@ -64,12 +58,10 @@ $p maxphisher $b
 $p colorama $b
 $p sockets $b
 $p requests $b
-echo "[**Baixando Ngrok...**]"
-$ngrok 
-echo "[**Download do Ngrok Concluido com Sucesso!!**]"
-echo "[**Baixando ApkTool para funcionamento do Metasploit!!**]"
-$m
-echo "[**Dependencias baixadas com sucesso**]"
+$p katana $b
+$p waybackurls $b
+$p hakrawler $b
+$px
 $c
 
 # Clonando repositório NucleiFuzzer e executando instalação
@@ -78,6 +70,7 @@ if [ ! -d "NucleiFuzzer" ]; then
   cd NucleiFuzzer
   sudo chmod +x install.sh && ./install.sh
   cd ..
+  nf
 else
   echo "[**Repositório NucleiFuzzer já clonado e pronto para funcionar!**]"
 fi
